c217b740df821e684ebedd4b2ffe2c88bf8657f3
QinGrain0 <linkther@163.com>
2022-01-24 14:45:24 +0800

    修改

--- 源.cpp
+++ 源.cpp
@@ -1,15 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
 
 int main()
 {
-	for (int i = 0;i <= 100; i++)
-	{
-		if (i % 2 != 0)
+	char arr[] = "happy new year !!!!!!!!!!!!!";
+	char m[] = "############################";
+	int left = 0; 
+	int right = strlen(arr)-1;
+		while(left <= right)
 		{
-			printf("当前奇数为：%d\n", i);
+			printf("%s\n", m);
+			m[left] = arr[left];
+			m[right] = arr[right];
+			left++;
+			right--;
+			Sleep(100);
+			system("cls");
 		}
-	}
+	//int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	//int i = 0, j,n = 0;
+	//scanf("%d", &n);
+	//for (i = 0; i < 10; i++)
+	//{
+	//	j = arr[i];
+	//	if (j == n)
+	//		printf("已找到数字%d",j);
+	//}
+	
+	 
+	//int x,y = 1,sum = 0;
+	//scanf("%d", &x);
+	//	for (int i = 1; i <= x; i++)
+	//	{
+	//		y = y*i;
+	//		sum = sum + y;
+	//	}	
+	//printf("%d\n", sum);
 	return 0;
 }
 
